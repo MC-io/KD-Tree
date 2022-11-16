c58e31902676156f202387b2ea4ca5334fade7a9
Moises <mcasaverde@unsa.edu.pe>
2022-11-15 21:30:57 -0500

    finish graphviz

--- main.py
+++ main.py
@@ -1,18 +1,26 @@
+"""
+Implementacion de un K-D Tree Clasico, con visualizacion incluida para dimension K = 2
+"""
+
 import pyglet
 from pyglet import shapes
+import os
 
 class Node:
+    # Constructor
     def __init__(self, vec):
         self.vec = vec
         self.left = None
         self.right = None
-        self.top_right = [600,600]
-        self.bot_left = [0,0]
+        self.top_right = [600,600]      # al conocer los limites del cuadrante del nodo
+        self.bot_left = [0,0]           # se pueden dibujar de manera mas facil con pyglet
 
 class KDTree:
-    def __init__(self, k):
+    def __init__(self, k, xbound, ybound):
         self.k = k
         self.root = None
+        self.xbound = xbound    # limites en x y y
+        self.ybound = ybound
     
     def insert(self, vec):
         if self.root == None:
@@ -31,7 +39,7 @@ class KDTree:
                 tmp = tmp.right
             height += 1
 
-        height -= 1
+        height -= 1         # La altura al finalizar el ciclo no es la que queremos para insertar el nuevo nodo
 
         if tmp2.vec[height % self.k] > new_node.vec[height % self.k]:
             tmp2.left = new_node
@@ -82,11 +90,46 @@ class KDTree:
     def print_in_order(self):
         self.prin_in_order(self.root)
 
-kd = 2;
-arbol = KDTree(kd)
+    def rec_dot(self,node,t,f):
+        if node is None:
+            return
+        f.write('{} [label="('.format(t));
+        for i in range(self.k):
+            f.write('{},'.format(node.vec[i]))
+        f.write(')"]\n');
 
-def drawBoard(shape_list, batch=None):
-    pre_order(arbol.root,0,shape_list,batch)
+        if node.left is not None:
+            f.write('{}  -> {} \n'.format(t,t*2)) 
+            self.rec_dot(node.left,t*2,f)
+        if node.right is not None:
+            f.write('{}  -> {} \n'.format(t,t*2+1))
+            self.rec_dot(node.right,t*2 + 1,f)
+        
+    
+    def create_dot(self):
+        f = open("kdtree.dot","w")
+        f.write("digraph {\n")
+        t = 1
+        f.write('{} [label="('.format(t));
+        for i in range(self.k):
+            f.write('{},'.format(self.root.vec[i]))
+        f.write(')"]\n');
+
+        if self.root.left is not None:
+            f.write('{}  -> {} \n'.format(t,t*2)) 
+            self.rec_dot(self.root.left,t*2,f)
+        if self.root.right is not None:
+            f.write('{}  -> {} \n'.format(t,t*2+1))
+            self.rec_dot(self.root.right,t*2 + 1,f)
+        f.write('}')
+        f.close()
+        #os.system('cmd /k "Graphviz/bin/dot -Tpng kdtree.dot -o kdtree.png"')
+
+
+    
+
+def drawBoard(tree,shape_list, batch=None):
+    pre_order(tree.root,0,shape_list,batch)
 
 def pre_order(node, depth, shape_list, batch):
     if node is None:
@@ -107,14 +150,27 @@ def pre_order(node, depth, shape_list, batch):
 
 
 if __name__ == "__main__":
+    kd = 2;
     xbound = 600
     ybound = 600
+    arbol = KDTree(kd,xbound,ybound)
     arbol.insert([350,420])
     arbol.insert([520,100])
     arbol.insert([320,237])
     arbol.insert([200,134])
     arbol.insert([50,450])
     arbol.insert([270,350])
+    arbol.insert([352,126])
+    arbol.insert([586,67])
+    arbol.insert([235,123])
+    arbol.insert([240,329])
+    arbol.insert([560,500])
+    arbol.insert([230,39])
+    arbol.insert([256,207])
+    arbol.insert([301,405])
+    arbol.insert([207,440])
+    arbol.insert([290,180])
+    arbol.insert([15,342])
 
     if arbol.search([7,46]) is not None:
         print("Si esta")
@@ -122,13 +178,14 @@ if __name__ == "__main__":
         print("No esta")
 
     arbol.print_in_order()
+    arbol.create_dot()
 
 
     window = pyglet.window.Window(600, 600, "Visualizacion 2-D Tree")
     batch = pyglet.graphics.Batch()
 
     shape_list = []
-    drawBoard(shape_list, batch=batch)
+    drawBoard(arbol,shape_list, batch=batch)
 
 
     @window.event
